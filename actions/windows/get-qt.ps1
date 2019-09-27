5b9c443f54e130812921b4e8090e2015ff462e9c
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-27 18:49:17 -0400

    Let's try downloading and running that, shall we?

--- actions/windows/get-qt.ps1
+++ actions/windows/get-qt.ps1
@@ -1,1 +1,4 @@
-Write-Output "get qt :)"
+Write-Output $Env:GITHUB_WORKSPACE
+(new-object System.Net.WebClient).DownloadFile('http://qt.mirror.constant.com/archive/qt/5.13/5.13.1/qt-opensource-windows-x86-5.13.1.exe',$Env:GITHUB_WORKSPACE + '/qt-install.exe')
+Start-Process -FilePath $Env:GITHUB_WORKSPACE -ArgumentList "--script", $Env:GITHUB_WORKSPACE + "/actions/installer-noninteractive.qs", "--platform minimal" -RedirectStandardOutput output.txt
+Get-Content output.txt | ForEach-Object {Write-Output $_}
