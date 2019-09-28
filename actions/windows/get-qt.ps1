aec9c4109d7d1a44e846cc9cbe1c0d7be2bca65f
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-27 21:15:47 -0400

    perhaps the offline installer will fare better

--- actions/windows/get-qt.ps1
+++ actions/windows/get-qt.ps1
@@ -1,6 +1,7 @@
 # $env:GITHUB_WORKSPACE = 'C:\Users\qbgee\Documents\Code\Aurora\Prototyping'
 
 Set-Location $env:GITHUB_WORKSPACE
-$installer_url = 'https://download.qt.io/archive/online_installers/3.1/qt-unified-windows-x86-3.1.1-online.exe'
+# $installer_url = 'https://download.qt.io/archive/online_installers/3.1/qt-unified-windows-x86-3.1.1-online.exe'
+$installer_url = 'https://download.qt.io/official_releases/qt/5.13/5.13.1/qt-opensource-windows-x86-5.13.1.exe'
 Invoke-WebRequest -Uri $installer_url -OutFile "qt-install.exe"
 .\qt-install.exe --script .\actions\installer-noninteractive-windows.qs --platform minimal --verbose
