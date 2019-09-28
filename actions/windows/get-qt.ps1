37d001a8ac8c2d523a9af2209140bdd616060929
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-27 21:43:25 -0400

    Let's try a different approach, shall we?

--- actions/windows/get-qt.ps1
+++ actions/windows/get-qt.ps1
@@ -3,4 +3,4 @@
 Set-Location $env:GITHUB_WORKSPACE
 $installer_url = 'https://download.qt.io/archive/online_installers/3.1/qt-unified-windows-x86-3.1.1-online.exe'
 Invoke-WebRequest -Uri $installer_url -OutFile "qt-install.exe"
-.\qt-install.exe --script .\actions\installer-noninteractive-windows.qs --verbose
+# .\qt-install.exe --script .\actions\installer-noninteractive-windows.qs --verbose
