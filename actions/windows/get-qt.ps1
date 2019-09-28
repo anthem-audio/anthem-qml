087fc2aa2178bac3504dbb3861d4a57da67d4867
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-27 23:21:30 -0400

    oh

--- actions/windows/get-qt.ps1
+++ actions/windows/get-qt.ps1
@@ -1,8 +1,8 @@
 # $env:GITHUB_WORKSPACE = 'C:\Users\qbgee\Documents\Code\Aurora\Prototyping'
 
 Set-Location $env:GITHUB_WORKSPACE
-$app = Get-WmiObject -Class Win32_Product -Filter "Name LIKE 'Microsoft Visual C++*'" | Write-output $app
-$app = Get-WmiObject -Class Win32_Product -Filter "Name LIKE 'Microsoft Visual C++*'" | foreach-object -process {$app.Uninstall()}
+$app = Get-WmiObject -Class Win32_Product -Filter "Name LIKE 'Microsoft Visual C++%'"
+foreach-object -process {$app.Uninstall()}
 $installer_url = 'https://download.qt.io/archive/online_installers/3.1/qt-unified-windows-x86-3.1.1-online.exe'
 Invoke-WebRequest -Uri $installer_url -OutFile "qt-install.exe"
 # .\qt-install.exe --script .\actions\installer-noninteractive-windows.qs --verbose
