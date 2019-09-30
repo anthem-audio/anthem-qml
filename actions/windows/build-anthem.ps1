957904df089569982791524f25ca0dafea333de7
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-30 14:54:06 -0400

    Correct path to qmake

--- actions/windows/build-anthem.ps1
+++ actions/windows/build-anthem.ps1
@@ -1,9 +1,5 @@
-Write-Host "Echo test 1"
 D:\Qt\5.13.1\mingw73_64\bin\qtenv2.bat
-Write-Host "Echo test 2"
 cd $env:GITHUB_WORKSPACE
-Write-Host "Echo test 3"
-qmake
-Write-Host "Echo test 4"
+D:\Qt\5.13.1\mingw73_64\bin\qmake
+Write-Host "Workspace after qmake:"
 dir $env:GITHUB_WORKSPACE
-Write-Host "Echo test 5"
