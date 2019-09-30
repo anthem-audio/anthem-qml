ecc7d1e793326ef1847f3448ea5d4b4bcc6c38ba
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-30 14:24:09 -0400

    I really just need to restart the tests

--- actions/windows/build-anthem.ps1
+++ actions/windows/build-anthem.ps1
@@ -1,4 +1,9 @@
+Write-Host "Echo test 1"
 D:\Qt\5.13.1\mingw73_64\bin\qtenv2.bat
+Write-Host "Echo test 2"
 cd %GITHUB_WORKSPACE%
+Write-Host "Echo test 3"
 qmake
+Write-Host "Echo test 4"
 dir %GITHUB_WORKSPACE%
+Write-Host "Echo test 5"
