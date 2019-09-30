2ae1c527b18e436aac47226e27feec55f2b7fb8c
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-30 15:15:18 -0400

    aaaaa

--- actions/windows/build-anthem.ps1
+++ actions/windows/build-anthem.ps1
@@ -1,5 +1,8 @@
 D:\Qt\5.13.1\mingw73_64\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
-D:\Qt\5.13.1\mingw73_64\bin\qmake -d
+D:\Qt\5.13.1\mingw73_64\bin\qmake
 Write-Host "Main folder after qmake:"
 dir $env:GITHUB_WORKSPACE\Main
+Write-Host "Folder above workspace after qmake:"
+cd ..
+dir
