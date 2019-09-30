b6a28fbf3e46d31ff0a09f86e06811734d5db753
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-30 15:04:20 -0400

    Let's try verbose output for qmake...

--- actions/windows/build-anthem.ps1
+++ actions/windows/build-anthem.ps1
@@ -1,5 +1,5 @@
 D:\Qt\5.13.1\mingw73_64\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
-D:\Qt\5.13.1\mingw73_64\bin\qmake
-Write-Host "Workspace after qmake:"
-dir $env:GITHUB_WORKSPACE
+D:\Qt\5.13.1\mingw73_64\bin\qmake -d
+Write-Host "Main folder after qmake:"
+dir $env:GITHUB_WORKSPACE\Main
