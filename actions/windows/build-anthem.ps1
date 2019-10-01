dce9ca93978b6a83a9b72870f9e4c9f29e6d269d
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-30 20:48:41 -0400

    Hopefully those DLLs won't be missing now

--- actions/windows/build-anthem.ps1
+++ actions/windows/build-anthem.ps1
@@ -7,6 +7,9 @@ mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
 cd Anthem
 D:\Qt\5.13.1\mingw73_64\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
+# Copy some extra DLLs that windeployqt missed (dlls might be because mingw wasn't run properly??)
+Copy-Item "D:\Qt\5.13.1\mingw73_64\bin\libgcc_s_seh-1.dll" -Destination "libgcc_s_seh-1.dll"
+Copy-Item "D:\Qt\5.13.1\mingw73_64\bin\libwinpthread-1.dll" -Destination "libwinpthread-1.dll"
+Copy-Item "D:\Qt\5.13.1\mingw73_64\bin\libstdc++-6.dll" -Destination "libstdc++-6.dll"
 cd ..
-7z a Anthem.zip Anthem
-Move-Item Anthem.zip D:\
+Move-Item Anthem -Destination D:\Anthem
