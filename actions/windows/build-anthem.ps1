c372540df5169d1205b36abf6b8cddbf0fb6d2cf
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-09-30 20:06:58 -0400

    If we're lucky, this will all just work

--- actions/windows/build-anthem.ps1
+++ actions/windows/build-anthem.ps1
@@ -1,8 +1,12 @@
 D:\Qt\5.13.1\mingw73_64\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
 D:\Qt\5.13.1\mingw73_64\bin\qmake
-Write-Host "Main folder after qmake:"
-dir $env:GITHUB_WORKSPACE\Main
-Write-Host "Folder above workspace after qmake:"
+D:\Qt\Tools\mingw730_64\bin\mingw32-make.exe
+cd Main\release
+mkdir Anthem
+Copy-Item Main.exe -Destination Anthem
+cd Anthem
+D:\Qt\5.13.1\mingw73_64\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
 cd ..
-dir
+7z a Anthem.zip Anthem
+Move-Item Anthem.zip $env:GITHUB_WORKSPACE
