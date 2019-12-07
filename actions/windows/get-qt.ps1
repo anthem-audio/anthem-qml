a63a7b6b10c17c8d84583aa3763794f151062c6d
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-12-07 18:42:12 -0500

    Download is hanging; try using curl.exe instead of Invoke-WebRequest

--- actions/windows/get-qt.ps1
+++ actions/windows/get-qt.ps1
@@ -1,6 +1,8 @@
 # $env:GITHUB_WORKSPACE = 'C:\Users\qbgee\Documents\Code\Aurora\Prototyping'
 
 Set-Location $env:GITHUB_WORKSPACE
+# choco install curl -y
 $installer_url = 'http://download.qt.io/official_releases/qt/5.13/5.13.2/qt-opensource-windows-x86-5.13.2.exe'
-Invoke-WebRequest -Uri $installer_url -OutFile "qt-install.exe"
+# Invoke-WebRequest -Uri $installer_url -OutFile "qt-install.exe"
+C:\Windows\System32\curl.exe $installer_url --output "qt-install.exe"
 # .\qt-install.exe --script .\actions\installer-noninteractive-windows.qs --verbose
