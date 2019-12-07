fad5a6c746cf565e30052eecdcddf6d44d8e7dbe
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-12-07 18:57:20 -0500

    what if we used Invoke-Command woaaa how wild would that beeee

--- actions/windows/get-qt.ps1
+++ actions/windows/get-qt.ps1
@@ -4,5 +4,5 @@ Set-Location $env:GITHUB_WORKSPACE
 # choco install curl -y
 $installer_url = 'http://download.qt.io/official_releases/qt/5.13/5.13.2/qt-opensource-windows-x86-5.13.2.exe'
 # Invoke-WebRequest -Uri $installer_url -OutFile "qt-install.exe"
-C:\Windows\System32\curl.exe $installer_url --output "qt-install.exe"
+Invoke-Command C:\Windows\System32\curl.exe $installer_url --output "qt-install.exe"
 # .\qt-install.exe --script .\actions\installer-noninteractive-windows.qs --verbose
