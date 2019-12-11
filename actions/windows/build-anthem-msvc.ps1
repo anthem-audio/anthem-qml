60e19fad7204cd7592ba67f88e413200982e8b0e
grejppi <grejppi@users.noreply.github.com>
2019-12-11 18:09:06 +0200

    Does this do anything...?

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -1,6 +1,6 @@
 cd $env:GITHUB_WORKSPACE
-D:\Qt\5.13.2\msvc2017_64\bin\qmake.exe
-D:\Qt\Tools\QtCreator\bin\jom.exe
+D:\Qt\5.13.2\msvc2017_64\bin\qmake.exe -o Makefile -spec win32-msvc
+D:\Qt\Tools\QtCreator\bin\jom.exe -f Makefile qmake_all
 cd Main\release
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
