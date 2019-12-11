5452a442df7388629190fedc36647b2e40bcd9c6
grejppi <grejppi@users.noreply.github.com>
2019-12-11 13:40:05 +0200

    OF COURSE

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -1,6 +1,7 @@
+D:\Qt\5.13.2\msvc2017_64\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
-D:\Qt\5.13.2\msvc2017_64\bin\qmake
-D:\Qt\5.13.2\msvc2017_64\bin\jom
+D:\Qt\5.13.2\msvc2017_64\bin\qmake.exe
+D:\Qt\5.13.2\msvc2017_64\bin\jom.exe
 cd Main\release
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
