9cd780167d4611aeb5960057f73fc13a156d2eed
grejppi <grejppi@users.noreply.github.com>
2019-12-11 19:02:24 +0200

    PowerShell more like PowerWhatTheHell

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -3,7 +3,11 @@ D:\Qt\5.13.2\msvc2017_64\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 D:\Qt\5.13.2\msvc2017_64\bin\qmake.exe
-D:\Qt\Tools\QtCreator\bin\jom.exe
+& {
+    # https://stackoverflow.com/a/12538601
+    $ErrorActionPreference = 'SilentlyContinue'
+    D:\Qt\Tools\QtCreator\bin\jom.exe 2>&1
+}
 cd Main\release
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
