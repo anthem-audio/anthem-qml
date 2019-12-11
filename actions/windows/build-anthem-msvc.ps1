37cbd1e9a7b449c14fed4b819bc98fe188743a3f
grejppi <grejppi@users.noreply.github.com>
2019-12-11 14:00:59 +0200

    Electric Boogaloo

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -1,5 +1,5 @@
 D:\Qt\5.13.2\msvc2017_64\bin\qtenv2.bat
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat"
 cd $env:GITHUB_WORKSPACE
 D:\Qt\5.13.2\msvc2017_64\bin\qmake.exe
 D:\Qt\5.13.2\msvc2017_64\bin\jom.exe
