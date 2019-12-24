d1234bef4282d4937b365d0a2c40fc081cd535c1
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-12-24 12:00:47 -0500

    not this again please

--- actions/windows/build-anthem-mingw.ps1
+++ actions/windows/build-anthem-mingw.ps1
@@ -2,6 +2,7 @@ D:\Qt\5.14.0\mingw73_64\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
 D:\Qt\5.14.0\mingw73_64\bin\qmake
 D:\Qt\Tools\mingw730_64\bin\mingw32-make.exe
+dir Main
 cd Main\release
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
