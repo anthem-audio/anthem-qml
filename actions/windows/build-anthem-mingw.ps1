7e24ca94d48d30587cd4cbbce2beba7a4d7f58c0
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-02-12 19:20:40 -0500

    it begins

--- actions/windows/build-anthem-mingw.ps1
+++ actions/windows/build-anthem-mingw.ps1
@@ -1,12 +1,12 @@
-.\$env:Qt5_Dir\5.14.0\mingw73_64\bin\qtenv2.bat
+Invoke-Expression $env:Qt5_Dir\5.14.0\mingw73_64\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
-.\$env:Qt5_Dir\5.14.0\mingw73_64\bin\qmake
-.\$env:Qt5_Dir\Tools\mingw730_64\bin\mingw32-make.exe
+Invoke-Expression $env:Qt5_Dir\5.14.0\mingw73_64\bin\qmake
+Invoke-Expression $env:Qt5_Dir\Tools\mingw730_64\bin\mingw32-make.exe
 cd Main\release
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
 cd Anthem
-.\$env:Qt5_Dir\5.14.0\mingw73_64\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
+Invoke-Expression $env:Qt5_Dir\5.14.0\mingw73_64\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
 # Copy some extra DLLs that windeployqt missed (dlls might be because mingw wasn't run properly??)
 Copy-Item "$env:Qt5_Dir\5.14.0\mingw73_64\bin\libgcc_s_seh-1.dll" -Destination "libgcc_s_seh-1.dll"
 Copy-Item "$env:Qt5_Dir\5.14.0\mingw73_64\bin\libwinpthread-1.dll" -Destination "libwinpthread-1.dll"
