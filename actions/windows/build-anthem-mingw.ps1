fa890da04ad691558abfe4809bfce41bb1480133
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-02-12 22:29:16 -0500

    aw come on it was supposed to work

--- actions/windows/build-anthem-mingw.ps1
+++ actions/windows/build-anthem-mingw.ps1
@@ -1,15 +1,15 @@
 # Invoke-Expression $env:Qt5_Dir\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
-Invoke-Expression $env:Qt5_Dir\bin\qmake
+$env:Qt5_Dir\bin\qmake
 choco uninstall mingw
 choco install mingw --version=7.3.0 -y
-Invoke-Expression C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin\mingw32-make.exe
+C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin\mingw32-make.exe
 # dir Main
 cd Main
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
 cd Anthem
-Invoke-Expression $env:Qt5_Dir\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
+$env:Qt5_Dir\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
 # Copy some extra DLLs that windeployqt missed (dlls might be because mingw wasn't run properly??)
 Copy-Item "$env:Qt5_Dir\bin\libgcc_s_seh-1.dll" -Destination "libgcc_s_seh-1.dll"
 Copy-Item "$env:Qt5_Dir\bin\libwinpthread-1.dll" -Destination "libwinpthread-1.dll"
