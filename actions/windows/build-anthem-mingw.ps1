eaa9eaa4d3c84c8251a5015fbe184d519c652195
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-02-12 21:51:30 -0500

    Let's try make with a real MinGW this time

--- actions/windows/build-anthem-mingw.ps1
+++ actions/windows/build-anthem-mingw.ps1
@@ -3,7 +3,7 @@ cd $env:GITHUB_WORKSPACE
 Invoke-Expression $env:Qt5_Dir\bin\qmake
 choco uninstall mingw
 choco install mingw --version=7.3.0 -y
-Invoke-Expression $env:Qt5_Dir\..\..\Tools\mingw730_64\bin\mingw32-make.exe
+Invoke-Expression C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin\mingw32-make.exe
 cd Main\release
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
