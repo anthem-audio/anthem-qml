7722e828cbd0aed1593cfe691bb0d56e539707c6
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-02-12 22:51:19 -0500

    boy oh boy what could go wrong

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -1,8 +1,8 @@
 . $env:GITHUB_WORKSPACE\actions\windows\Invoke-CmdScript.ps1
-D:\Qt\5.14.0\msvc2017_64\bin\qtenv2.bat
+# D:\Qt\5.14.0\msvc2017_64\bin\qtenv2.bat
 cd $env:GITHUB_WORKSPACE
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
-D:\Qt\5.14.0\msvc2017_64\bin\qmake.exe -spec win32-msvc
+. $env:Qt5_Dir\bin\qmake.exe -spec win32-msvc
 & {
     # https://stackoverflow.com/a/12538601
     $ErrorActionPreference = 'SilentlyContinue'
@@ -12,6 +12,6 @@ cd Main\release
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
 cd Anthem
-D:\Qt\5.14.0\msvc2017_64\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
+. $env:Qt5_Dir\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
 cd ..
 Move-Item Anthem -Destination D:\Anthem
