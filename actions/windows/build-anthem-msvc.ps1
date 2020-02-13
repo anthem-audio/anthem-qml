c6c4b3fa188532cbfd6c6bd73322ff69720e8158
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-02-13 00:47:04 -0500

    ok I'm done if this isn't working in 15 minutes

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -3,14 +3,14 @@
 cd $env:GITHUB_WORKSPACE
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 . $env:Qt5_Dir\bin\qmake.exe -spec win32-msvc
-# & {
-#     # https://stackoverflow.com/a/12538601
-#     $ErrorActionPreference = 'SilentlyContinue'
-#     D:\Qt\Tools\QtCreator\bin\jom.exe 2>&1
-# }
 cd Main
+& {
+    # https://stackoverflow.com/a/12538601
+    $ErrorActionPreference = 'SilentlyContinue'
+    # D:\Qt\Tools\QtCreator\bin\jom.exe 2>&1
+    nmake 2>&1
+}
 # tree "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\bin"
-nmake
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
 cd Anthem
