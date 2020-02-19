21dad347ac44d961bca49acdb551e914892def74
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-02-18 22:16:01 -0500

    well I haven't tried this specific incantation yet

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -5,13 +5,13 @@ cd $env:GITHUB_WORKSPACE
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 . $env:Qt5_Dir\bin\qmake.exe -spec win32-msvc
 cd Main
-nmake
-# & {
-#     # https://stackoverflow.com/a/12538601
-#     $ErrorActionPreference = 'SilentlyContinue'
-#     # D:\Qt\Tools\QtCreator\bin\jom.exe 2>&1
-#     nmake 2>&1
-# }
+# nmake
+& {
+    # https://stackoverflow.com/a/12538601
+    $ErrorActionPreference = 'Continue'
+    # D:\Qt\Tools\QtCreator\bin\jom.exe 2>&1
+    nmake
+}
 # tree "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\bin"
 mkdir Anthem
 Copy-Item Main.exe -Destination Anthem
