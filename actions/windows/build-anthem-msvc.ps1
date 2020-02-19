4ef741563e42dc59c49bdd655bb1b015ebfc0b4f
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-02-18 23:29:08 -0500

    uh probably

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -1,22 +1,16 @@
 . $env:GITHUB_WORKSPACE\actions\windows\Invoke-CmdScript.ps1
-# . $env:Qt5_Dir\bin\qtenv2.bat
-# $env:Path = "$env:Qt5_Dir\bin;" + $env:Path
 cd $env:GITHUB_WORKSPACE
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 cd Main
 . $env:Qt5_Dir\bin\qmake.exe
-# dir
-# nmake
 & {
     # https://stackoverflow.com/a/12538601
     $ErrorActionPreference = 'Continue'
     # D:\Qt\Tools\QtCreator\bin\jom.exe 2>&1
     nmake
 }
-# tree "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\bin"
 mkdir Anthem
-tree /f
-Copy-Item debug\Anthem\Main.exe -Destination Anthem
+Copy-Item release\Main.exe -Destination Anthem
 cd Anthem
 . $env:Qt5_Dir\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
 cd ..
