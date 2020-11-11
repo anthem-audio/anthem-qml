fcfa208a3acd6bb9b385724af82c6cd229cd729c
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-11-11 17:53:28 -0500

    Attempt to fix builds (it begins :)

--- actions/windows/build-anthem-mingw.ps1
+++ actions/windows/build-anthem-mingw.ps1
@@ -1,16 +1,16 @@
 cd $env:GITHUB_WORKSPACE
 . $env:Qt5_Dir\bin\qmake
 
-choco uninstall mingw
-choco install mingw --version=7.3.0 -y
+# choco uninstall mingw
+# choco install mingw --version=7.3.0 -y
 
 C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin\mingw32-make.exe
 
-cd Main
-mkdir Anthem
-Copy-Item Main.exe -Destination Anthem
 cd Anthem
-. $env:Qt5_Dir\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
+mkdir AnthemBuild
+Copy-Item Anthem.exe -Destination Anthem
+cd AnthemBuild
+. $env:Qt5_Dir\bin\windeployqt.exe Anthem.exe --qmldir $env:GITHUB_WORKSPACE\Anthem
 
 # Copy some extra DLLs that windeployqt missed (dlls might be because mingw wasn't run properly??)
 Copy-Item "$env:Qt5_Dir\bin\libgcc_s_seh-1.dll" -Destination "libgcc_s_seh-1.dll"
@@ -18,4 +18,4 @@ Copy-Item "$env:Qt5_Dir\bin\libwinpthread-1.dll" -Destination "libwinpthread-1.d
 Copy-Item "$env:Qt5_Dir\bin\libstdc++-6.dll" -Destination "libstdc++-6.dll"
 
 cd ..
-Move-Item Anthem -Destination D:\Anthem
+Move-Item AnthemBuild -Destination D:\Anthem
--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -1,16 +1,16 @@
 . $env:GITHUB_WORKSPACE\actions\windows\Invoke-CmdScript.ps1
 cd $env:GITHUB_WORKSPACE
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
-cd Main
+cd Anthem
 . $env:Qt5_Dir\bin\qmake.exe
 & {
     # https://stackoverflow.com/a/12538601
     $ErrorActionPreference = 'Continue'
     nmake
 }
-mkdir Anthem
-Copy-Item release\Main.exe -Destination Anthem
-cd Anthem
+mkdir AnthemBuild
+Copy-Item release\Anthem.exe -Destination AnthemBuild
+cd AnthemBuild
 . $env:Qt5_Dir\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
 cd ..
-Move-Item Anthem -Destination D:\Anthem
+Move-Item AnthemBuild -Destination D:\Anthem
