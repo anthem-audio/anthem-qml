5e68461da5c72597b8b215251ddd1f1b16bef799
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-11-12 10:57:52 -0500

    Use Qt5 directory environment variable

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -4,7 +4,7 @@ Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise
 cd src
 # ls "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise" "cl.exe" -Recurse -File
 # cmake . -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_C_COMPILER:STRING="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.27.29110\bin\HostX86\x64\cl.exe" -DCMAKE_PREFIX_PATH:STRING="C:\Qt\5.15.1\msvc2019_64" -DQT_QMAKE_EXECUTABLE:STRING="C:\Qt\5.15.1\msvc2019_64\bin\qmake.exe"
-cmake . -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_C_COMPILER:STRING="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.27.29110\bin\HostX64\x64\cl.exe" -DCMAKE_PREFIX_PATH:STRING="C:\Qt\5.15.1\msvc2019_64" -DQT_QMAKE_EXECUTABLE:STRING="C:\Qt\5.15.1\msvc2019_64\bin\qmake.exe"
+cmake . -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_C_COMPILER:STRING="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.27.29110\bin\HostX64\x64\cl.exe" -DCMAKE_PREFIX_PATH:STRING=$env:Qt5_Dir\msvc2019_64 -DQT_QMAKE_EXECUTABLE:STRING=$env:Qt5_Dir\msvc2019_64\bin\qmake.exe
 & {
     # https://stackoverflow.com/a/12538601
     $ErrorActionPreference = 'Continue'
