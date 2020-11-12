1c0688b5e7407c0f39e63c75208a290939e46056
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-11-12 12:33:25 -0500

    Fix src location for packaging and clean up

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -1,28 +1,20 @@
-Write-Host "`nBuild script started.`n`n"
-Write-Host "`nInstalling Invoke-CmdScript...`n`n"
+Write-Host "Build script started."
+Write-Host "Installing Invoke-CmdScript..."
 . $env:GITHUB_WORKSPACE\actions\windows\Invoke-CmdScript.ps1
 
 cd $env:GITHUB_WORKSPACE\src
-Write-Host "`nSetting up MSVC build environment...`n`n"
+Write-Host "Setting up MSVC build environment..."
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 
-# cmake . -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_C_COMPILER:STRING="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.27.29110\bin\HostX86\x64\cl.exe" -DCMAKE_PREFIX_PATH:STRING="C:\Qt\5.15.1\msvc2019_64" -DQT_QMAKE_EXECUTABLE:STRING="C:\Qt\5.15.1\msvc2019_64\bin\qmake.exe"
-Write-Host "`nRunning CMake...`n`n"
-# cmake . -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_C_COMPILER:STRING="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.27.29110\bin\HostX64\x64\cl.exe" -DCMAKE_PREFIX_PATH:STRING=$env:Qt5_Dir\msvc2019_64 -DQT_QMAKE_EXECUTABLE:STRING=$env:Qt5_Dir\msvc2019_64\bin\qmake.exe
+Write-Host "Running CMake...`n`n"
 cmake .
 
 Write-Host "`nBuilding...`n`n"
 msbuild Anthem.sln /property:Configuration=Release
-# & {
-#     # https://stackoverflow.com/a/12538601
-#     $ErrorActionPreference = 'Continue'
-#     nmake
-# }
 mkdir AnthemBuild
-ls . "Anthem.exe" -Recurse -File
-Copy-Item release\Anthem.exe -Destination AnthemBuild
+Copy-Item Release\Anthem.exe -Destination AnthemBuild
 cd AnthemBuild
 Write-Host "`nPackaging...`n`n"
-. $env:Qt5_Dir\bin\windeployqt.exe Anthem.exe --qmldir $env:GITHUB_WORKSPACE\Main
+. $env:Qt5_Dir\bin\windeployqt.exe Anthem.exe --qmldir $env:GITHUB_WORKSPACE\src
 cd ..
 Move-Item AnthemBuild -Destination D:\Anthem
