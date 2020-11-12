37b1751a9931c2098e260b873f5a14e8eed018fb
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-11-12 10:19:11 -0500

    DIR

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -2,7 +2,7 @@
 cd $env:GITHUB_WORKSPACE
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
 cd src
-dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\"
+dir "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise"
 cmake . -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_C_COMPILER:STRING="C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.24.28314\bin\HostX86\x64\cl.exe" -DCMAKE_PREFIX_PATH:STRING="C:\Qt\5.15.1\msvc2019_64" -DQT_QMAKE_EXECUTABLE:STRING="C:\Qt\5.15.1\msvc2019_64\bin\qmake.exe"
 & {
     # https://stackoverflow.com/a/12538601
