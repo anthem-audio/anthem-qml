7ab59446ba86d029d9d2d1cfd05c001951199c7a
Joshua Wade <SecondFlight@users.noreply.github.com>
2020-11-13 14:11:35 -0500

    Convert to CMake and update CLI (#78)

--- actions/windows/build-anthem-msvc.ps1
+++ actions/windows/build-anthem-msvc.ps1
@@ -1,16 +1,20 @@
+Write-Host "Build script started."
+Write-Host "Installing Invoke-CmdScript..."
 . $env:GITHUB_WORKSPACE\actions\windows\Invoke-CmdScript.ps1
-cd $env:GITHUB_WORKSPACE
+
+cd $env:GITHUB_WORKSPACE\src
+Write-Host "Setting up MSVC build environment..."
 Invoke-CmdScript "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" amd64
-cd Main
-. $env:Qt5_Dir\bin\qmake.exe
-& {
-    # https://stackoverflow.com/a/12538601
-    $ErrorActionPreference = 'Continue'
-    nmake
-}
-mkdir Anthem
-Copy-Item release\Main.exe -Destination Anthem
-cd Anthem
-. $env:Qt5_Dir\bin\windeployqt.exe Main.exe --qmldir $env:GITHUB_WORKSPACE\Main
+
+Write-Host "Running CMake...`n`n"
+cmake .
+
+Write-Host "`nBuilding...`n`n"
+msbuild Anthem.sln /property:Configuration=Release
+mkdir AnthemBuild
+Copy-Item Release\Anthem.exe -Destination AnthemBuild
+cd AnthemBuild
+Write-Host "`nPackaging...`n`n"
+. $env:Qt5_Dir\bin\windeployqt.exe Anthem.exe --qmldir $env:GITHUB_WORKSPACE\src
 cd ..
-Move-Item Anthem -Destination D:\Anthem
+Move-Item AnthemBuild -Destination D:\Anthem
